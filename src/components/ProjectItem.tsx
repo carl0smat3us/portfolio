79acd3a108919f9213a918e05c3489ce91eefa24
Carlos <carlosmatateumateus@gmail.com>
2024-10-16 16:04:05 +0000

    feat: Ending to add the projects informations

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,9 +1,11 @@
 import { Project } from "../@types";
 
-export default function ProjectItem({ id }: Project) {
+export default function ProjectItem({ id, cover }: Project) {
   return (
     <a href={`/project/${id}`}>
-      <div className="skeleton border border-black h-64 w-full bg-[rgb(233,233,233)]" />
+      <div className="border border-black h-64 w-full bg-[rgb(233,233,233)] bg-cover" style={{
+        backgroundImage: `url(${cover})`
+      }}/>
     </a>
   );
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,8 +2,8 @@ import { Project } from "../@types";
 
 export const PROJECTS:Project[] = [
   {
-    id: 1,
-    title: "Priva domicilio lading page",
+    id: "privadodomicilio",
+    title: "Privado Domicílio lading page",
     description: "Privado Domicílio is committed to providing specialized and personalized care for seniors and individuals in need, right in the comfort of their own homes. With a dedicated and caring team, we strive to ensure the well-being and quality of life of our clients with every service we provide.",
     skills: [
       "React JS", "Next.JS", "Tailwind CSS", "Swiper JS", "Javascript", "TypeScript"
@@ -16,5 +16,37 @@ export const PROJECTS:Project[] = [
       }
     ],
     preview: "https://www.privadodomicilio.com/"
+  },
+  {
+    id: "raciuscare",
+    title: "Racius Care",
+    description: "Racius Care is an online store specializing in health and hygiene, focused on promoting well-being. It offers quality products and exceptional customer service.",
+    skills: [
+      "Ecommerce", "Ecommerce Website", "Firebase", "Swiper JS", "JavaScript", "Google Cloud Platform", "TypeScript", "React", "Python", "Flask"
+    ],
+    cover: "/projects/raciuscare/cover.png",
+    photos: [
+      {
+        path: "/projects/raciuscare/home.png",
+        label: "Home"
+      },
+      {
+        path: "/projects/raciuscare/product.png",
+        label: "Product page"
+      },
+      {
+        path: "/projects/raciuscare/cart.png",
+        label: "Products cart"
+      },
+      {
+        path: "/projects/raciuscare/cart.png",
+        label: "Products cart"
+      },
+      {
+        path: "/projects/raciuscare/dashboard.png",
+        label: "Admin dashboard"
+      }
+    ],
+    preview: "https://www.raciuscare.com/"
   }
 ]
